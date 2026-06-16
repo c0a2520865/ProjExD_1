d34c0c209413bfeedc93e87f71b9328ce572e795
reina <c0a2520865@edu.teu.ac.jp>
2026-06-16 18:09:06 +0900

    演習課題

---
+++ pygame_challenge.py
@@ -0,0 +1,58 @@
+import os
+import sys
+import pygame as pg
+
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
+def main():
+    pg.display.set_caption("はばたけ！こうかとん（演習課題）")
+    screen = pg.display.set_mode((1600, 900))
+    clock = pg.time.Clock()
+    tmr = 0
+
+    bg_img = pg.image.load("fig/pg_bg.jpg") 
+    bg_img_flip = pg.transform.flip(bg_img, True, False)
+
+    kk_img = pg.image.load("fig/3.png")             
+    kk_img = pg.transform.flip(kk_img, True, False) 
+
+    kk_rct = kk_img.get_rect()
+    kk_rct.center = 300, 200
+
+    while True:
+        for event in pg.event.get():
+            if event.type == pg.QUIT: return
+
+        x = tmr % 3200
+
+        screen.blit(bg_img, [-x, 0])
+        screen.blit(bg_img_flip, [1600 - x, 0])
+        screen.blit(bg_img, [3200 - x, 0])
+
+        key_lst = pg.key.get_pressed()
+
+        move_x = -1
+        move_y = 0
+
+        if key_lst[pg.K_UP]:
+            move_y = -1
+        if key_lst[pg.K_DOWN]:
+            move_y = 1
+        if key_lst[pg.K_LEFT]:
+            move_x = -2  
+        if key_lst[pg.K_RIGHT]:
+            move_x = 1  
+
+        kk_rct.move_ip((move_x, move_y))
+
+        screen.blit(kk_img, kk_rct)
+
+        pg.display.update()
+        tmr += 1        
+        clock.tick(200)
+
+if __name__ == "__main__":
+    pg.init()
+    main()
+    pg.quit()
+    sys.exit()
