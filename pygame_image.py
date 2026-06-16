b9cd3d807b45699f51a20e85022275ab68d15a46
reina <c0a2520865@edu.teu.ac.jp>
2026-06-16 16:49:04 +0900

    練習１と２：背景画像の描画

--- pygame_image.py
+++ pygame_image.py
@@ -6,19 +6,34 @@ os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
 
 def main():
-    pg.display.set_caption("はばたけ！こうかとん")
+    pg.display.set_caption("はじめてのPygame")
     screen = pg.display.set_mode((800, 600))
-    clock  = pg.time.Clock()
-    bg_img = pg.image.load("fig/pg_bg.jpg")
+    clock = pg.time.Clock()
+    font = pg.font.Font(None, 80)
+
+    enn = pg.Surface((20, 20))
+    pg.draw.circle(enn, (255, 0, 0), (10, 10), 10)
+    enn.set_colorkey((0, 0, 0))
+
     tmr = 0
+    bg_img = pg.image.load("fig/pg_bg.jpg")
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
-        screen.blit(bg_img, [0, 0])
+        screen.blit(bg_img,[0,0])
+
+        pg.display.update()
+        tmr += 1
+        clock.tick(1)
+
+        txt = font.render(str(tmr), True, (255, 255, 255))
+        screen.fill((50, 50, 50))
+        screen.blit(txt, [300, 200])
+        screen.blit(enn, [100, 400])
         pg.display.update()
         tmr += 1        
-        clock.tick(10)
+        clock.tick(1)
 
 
 if __name__ == "__main__":
